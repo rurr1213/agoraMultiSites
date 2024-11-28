6b59d490838e4840565f52359627d4deb0b8bd7f
Ravi <rurr1213@gmail.com>
2024-11-28 02:25:36 +0000

    working!

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,23 @@
 # Your custom commands here (e.g., create a directory, install a plugin)
 echo "Running custom startup script..."
 mkdir -p /var/www/html/custom-directory
-echo "After creating directory..."
+
 # Add any other command, such as installing plugins via wp-cli
+echo "Setup for certbot"
+apt update -y
+apt install python3 python3-venv libaugeas0 -y
+apt-get remove certbot -y
+python3 -m venv /opt/certbot/
+/opt/certbot/bin/pip install --upgrade pip
+/opt/certbot/bin/pip install certbot certbot-apache
+ln -s /opt/certbot/bin/certbot /usr/bin/certbot
+
+apt install vim -y
+apt install -y iputils-ping
+
+# Setup sites
+a2ensite 000-default-le-ssl.conf
+a2enmod ssl
 
 # Start the original WordPress entrypoint
 echo "About to start wordpress"
