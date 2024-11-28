c7f083939d4a564d9a7c8138c85c64eb18b0c78c
Ravi <rurr1213@gmail.com>
2024-11-28 04:59:25 +0000

    small mods

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 # Your custom commands here (e.g., create a directory, install a plugin)
 echo "Running custom startup script..."
-mkdir -p /var/www/html/custom-directory
 
-# Add any other command, such as installing plugins via wp-cli
+#--------------------------------------------------
+# Add setup for certbot
 echo "Setup for certbot"
 apt update -y
 apt install python3 python3-venv libaugeas0 -y
@@ -14,11 +14,16 @@ python3 -m venv /opt/certbot/
 /opt/certbot/bin/pip install certbot certbot-apache
 ln -s /opt/certbot/bin/certbot /usr/bin/certbot
 
+#--------------------------------------------------
+# Add some tools
 apt install vim -y
 apt install -y iputils-ping
 
+#--------------------------------------------------
+# apach2 setup
 # Setup sites
 a2ensite 000-default-le-ssl.conf
+a2ensite 000-default.conf
 a2enmod ssl
 
 # Start the original WordPress entrypoint
