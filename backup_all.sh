725bf641b49bba346dea73985ec586d86ae88463
Ravi <rurr1213@gmail.com>
2024-11-30 04:09:51 +0000

    all backed up

--- backup_all.sh
+++ backup_all.sh
@@ -4,5 +4,8 @@
 
 docker compose exec db bash -c 'rm -rf /backup/*'
 docker compose exec db bash -c '/usr/bin/mariadb-backup --backup --target-dir=/backup --user=root --password=password'
+docker compose exec db bash -c 'tar -czvf /backup/db_backup_archive.tar.gz --mode=a+r /backup/*'
+docker compose cp db:/backup/db_backup_archive.tar.gz db_backup/db_backup_archive.tar.gz
+
 
 sudo tar -czvf /home/ubuntu/dev/agoraMultiSites/wordpress_backup/wordpress_backup.tar.gz  --exclude=/home/ubuntu/dev/agoraMultiSites/wordpress_files/wp-content/uploads /home/ubuntu/dev/agoraMultiSites/wordpress_files
